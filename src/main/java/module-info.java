ae5e0329737e28fbc71b78b44cdb4ff3227c4da4
Weiter4568 <90168326+Weiter4568@users.noreply.github.com>
2021-12-13 21:30:43 +0800

    Create module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.othello {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.othello to javafx.fxml;
+    exports com.example.othello;
+}
